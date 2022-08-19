3ef2a884cd43a9632e0bee0ddecc9421a99a5ab9
Adam Chlus <23018310+adamchlus@users.noreply.github.com>
2022-08-19 07:39:52 -0700

    Changed python version

--- .imgspec/install.sh
+++ .imgspec/install.sh
@@ -3,7 +3,7 @@ imgspec_dir=$(cd "$(dirname "$0")" ; pwd -P)
 pge_dir=$(dirname ${imgspec_dir})
 
 # Need to do custom install to prevent dependency errors
-conda create -y --name sister python=3.8
+conda create -y --name sister python=3.7
 source activate sister
 git clone https://github.com/EnSpec/hytools.git
 cd hytools
