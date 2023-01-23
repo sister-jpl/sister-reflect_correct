8d02f237020ecfbfa3d9980380b1f1aa1eaaacff
Adam Chlus <23018310+adamchlus@users.noreply.github.com>
2023-01-23 14:02:22 -0800

    Updated install script and algorithm yaml

--- install.sh
+++ install.sh
@@ -1,13 +1,11 @@
-run_dir=$('pwd')
-imgspec_dir=$(cd "$(dirname "$0")" ; pwd -P)
-pge_dir=$(dirname ${imgspec_dir})
+pge_dir=$(cd "$(dirname "$0")" ; pwd -P)
 
 # Need to do custom install to prevent dependency errors
-conda create -y --name sister python=3.7
+conda create -y --name sister python=3.8
 source activate sister
-
-pip install -r ${pge_dir}/requirements.txt
 
 git clone https://github.com/EnSpec/hytools.git
 cd hytools
 pip install .
+
+pip install -r ${pge_dir}/requirements.txt
